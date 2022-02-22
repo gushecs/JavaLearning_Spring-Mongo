c467b2b583d8ca08b6980235c6958fd2b2110d4d
Gustavo Santos <gustavo.heitor@outlook.com>
2022-02-22 13:20:14 -0300

    Post entity

--- src/main/java/com/example/s26/config/Instantiation.java
+++ src/main/java/com/example/s26/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.example.s26.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.example.s26.domain.Post;
 import com.example.s26.domain.User;
+import com.example.s26.repositories.PostRepository;
 import com.example.s26.repositories.UserRepository;
 
 @Configuration
@@ -14,17 +18,29 @@ public class Instantiation implements CommandLineRunner{
 	
 	@Autowired
 	UserRepository userRepository;
+	
+	@Autowired
+	PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post p1 = new Post(null,sdf.parse("21/03/2018"),"Partiu viagem!","Vou viajar para SP, abraços!",maria);
+		Post p2 = new Post(null,sdf.parse("23/03/2018"),"Bom dia","Acordei feliz hoje :)",maria);
+		
+		
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		postRepository.saveAll(Arrays.asList(p1,p2));
 		
 	}
 
---
+++ src/main/java/com/example/s26/repositories/PostRepository.java
@@ -0,0 +1,10 @@
+package com.example.s26.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.s26.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
