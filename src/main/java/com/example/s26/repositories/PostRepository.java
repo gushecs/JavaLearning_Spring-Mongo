b62183e69dd04d2e6fbaecf5d12e25985155c9a2
Gustavo Santos <gustavo.heitor@outlook.com>
2022-02-22 14:23:24 -0300

    consult posts by title

--- src/main/java/com/example/s26/repositories/PostRepository.java
+++ src/main/java/com/example/s26/repositories/PostRepository.java
@@ -1,5 +1,7 @@
 package com.example.s26.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import com.example.s26.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
