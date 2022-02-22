cedaf3e5fac27d49d3191070d288fe952e254180
Gustavo Santos <gustavo.heitor@outlook.com>
2022-02-22 11:27:39 -0300

    insert user

--- src/main/java/com/example/s26/services/UserService.java
+++ src/main/java/com/example/s26/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.s26.DTO.UserDTO;
 import com.example.s26.domain.User;
 import com.example.s26.repositories.UserRepository;
 import com.example.s26.services.exception.ObjectNotFoundException;
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(),objDTO.getName(),objDTO.getEmail());
 	}
 
 }
